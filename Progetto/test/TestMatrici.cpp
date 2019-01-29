87e95998d40d65a7ad2d114852f6569948df27bc
benedettagiorgi <40075404+benedettagiorgi@users.noreply.github.com>
2019-01-29 10:28:24 +0100

    Update TestMatrici.cpp

--- Progetto/test/TestMatrici.cpp
+++ Progetto/test/TestMatrici.cpp
@@ -11,6 +11,7 @@ TEST(Costruttore, RigheColonne) {
 
 }
 
+
 TEST(Costruttore, Vettore) {
     Matrice<int> B(2,3, {2, 3, 1, 4, 2, 1});
 
@@ -23,4 +24,38 @@ TEST(Costruttore, Vettore) {
     ASSERT_EQ(1, B.getValore(1,2));
 
 
+}
+
+TEST(Selezione, SelezionaRiga) {
+    Matrice<int> B(2,3, {2, 3, 1, 4, 2, 1});
+    ASSERT_THROW(B.selezionaRiga(4), eccezioneIndice);
+
+}
+
+TEST(Selezione, SelezionaColonna) {
+    Matrice<int> B(2,3, {2, 3, 1, 4, 2, 1});
+    ASSERT_THROW(B.selezionaColonna(5), eccezioneIndice);
+}
+
+TEST(Costruttore, DimensioniErrate) {
+    ASSERT_THROW(Matrice<int>C(2,4, {1,2,5}), eccezioneIndice);
+
+}
+
+TEST(Costruttore, NumRighe0) {
+
+    ASSERT_THROW(Matrice<int>(0,5, {1,4,7}) , eccezioneIndice);
+    ASSERT_THROW(Matrice<int>(0,13), eccezioneIndice);
+}
+
+
+TEST(Costruttore, NumColonne0) {
+    ASSERT_THROW(Matrice<int>(7,0, {1,4,7,10,15,9,0}) , eccezioneIndice);
+    ASSERT_THROW(Matrice<int>(9,0), eccezioneIndice);
+}
+
+
+TEST(getValore, IndiciErrati) {
+    Matrice<int>C(3,7);
+    ASSERT_THROW(C.getValore(5,2), eccezioneIndice);
 }
